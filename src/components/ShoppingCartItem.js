f9f8c818506da7cb1a78d8e38d0ecb270d583eb3
Jie Zhang <zhangjiew@gmail.com>
2021-08-12 13:55:16 -0600

    mvp done

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import ProductContext from "../contexts/ProductContext";
 
-const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+const Item = (props) => {
+  const { removeItem } = useContext(ProductContext);
+  const removeItemHandler = () => {
+    removeItem(props.id);
+  };
 
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.image} alt={`${props.title} book`} />
 
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <h1>{props.title}</h1>
+        <p>$ {props.price}</p>
+        <button onClick={removeItemHandler}>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
